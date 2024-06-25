4ea0fef80e19591ac53521a87f9d6cb7b60748f6
Karthik <krtdvn@gmail.com>
2024-06-25 15:58:11 +0530

    editor

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 PUB_URL = "https://docs.google.com/document/d/e/2PACX-1vQahx_xjl9XefTgAhA4KTjmfTjB0lV0k62dRhZlVrIEYBJyEZ9DGNlToGoAafGFbviSr7nf0JYH9irZ/pub"
+EDIT_URL = "https://docs.google.com/document/d/1iR5WvnL874ld7ioESzVk63fm5dOr8XGOy8NyUSV5TCQ/edit"
+BUILD_HOOK = "https://api.netlify.com/build_hooks/667a9250b506a1d355b792e7"
--- generate.py
+++ generate.py
@@ -4,11 +4,19 @@ import requests
 from bs4 import BeautifulSoup
 
 os.makedirs("build/", exist_ok=True)
+
 soup = BeautifulSoup(requests.get(config.PUB_URL).text, features="lxml")
 title = soup.select_one("title").text
 content = soup.select_one("div.doc-content").decode_contents()
 template = open("template.html").read()
 render = template.replace("{{title}}", title).replace("{{content}}", content)
 f = open("build/index.html", "w")
+f.write(render)
+f.close()
+
+os.makedirs("build/edit/", exist_ok=True)
+editor_template = open("edit.html").read()
+render = editor_template.replace("{{BUILD_HOOK}}", config.BUILD_HOOK).replace("{{EDIT_URL}}", config.EDIT_URL)
+f = open("build/edit/index.html", "w")
 f.write(render)
 f.close()
